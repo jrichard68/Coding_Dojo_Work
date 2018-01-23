65067e4df4ece19f78b79ee4e3494f2719183eba
Jamie Richard <jrichard68@live.com>
2018-01-23 12:46:46 -0800

    update

--- Web_Fundamentals/JavaScript/Fundamentals/randomChance.js
+++ Web_Fundamentals/JavaScript/Fundamentals/randomChance.js
@@ -1,11 +1,10 @@
 
 function slots(numQuarters) {
-    var winningNum = Math.floor;
-    var winNumCoins = Math.floor;
+    var winningNum = Math.floor;(Math.random() * 100);
+    var winNumCoins = Math.floor;(Math.random() * 50)+50;
     while(numQuarters > 0){
-        (Math.random() * 100);
         if(winningNum == 1) {
-            (Math.random() * 50)+50;
+            
             return winNumCoins + numQuarters;
         }
         else {
